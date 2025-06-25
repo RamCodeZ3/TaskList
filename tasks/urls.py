7af2bb29a8c5362401d577f89c1e301ac65fbfce
CodeFoxZ37 <130168476+CodeFoxZ37@users.noreply.github.com>
2025-06-24 21:13:24 -0400

    se crearon las api basica de get, postdelete, put

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,6 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('getTask/', views.TaskViewSet.as_view(), name='get-tasks'),
-    path('createTask/', views.TaskCreateView.as_view(), name='create-task'), # Include the tasks app URLs
+    path('createTask/', views.TaskCreateView.as_view(), name='create-task'),
+    path('deleteTask/<int:id>', views.TaskDeleteView.as_view(), name='delete-task'),  # Include the tasks app URLs
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,9 +3,10 @@ from rest_framework import viewsets
 from .serializer import TaskSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 class TaskViewSet(APIView):
-    def get_task(self, request):
+    def get(self, request):
         queryset = TaskTable.objects.all()
         serializer = TaskSerializer(queryset, many=True)
         
@@ -16,6 +17,26 @@ class TaskViewSet(APIView):
 class TaskCreateView(APIView):
     def post(self, request):
         serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
+class TaskDeleteView(APIView):
+    def delete(self,request,id):
+        try:
+            task = TaskTable.objects.get(pk=id)
+            task.delete()
+            task.save()
+            return Response({"message": "Task deleted successfully"}, status=204)
+        except TaskTable.DoesNotExist:
+            return Response({"error": "Task not found"}, status=404)
+
+class TaskUpdateView(APIView):
+    def put(self,request,id):
+            
+        task = get_object_or_404(TaskTable, pk=id)
+        serializer = TaskSerializer(task, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
