9cf99245c927ef8834981142d59e9363c8790fd7
RamCodeZ3 <arammusset7@gmail.com>
2025-10-12 21:09:45 -0400

    Se creo el componete header con sus estilos y la carpeta common

---
+++ frotend/src/common/Header.tsx
@@ -0,0 +1,21 @@
+import { BookCheck, Plus } from 'lucide-react';
+
+function Header(){
+    return(
+        <>
+        <div className='flex items-center justify-center w-[100%] bg-neutral-900 text-white py-4'>
+            <header className='flex justify-between w-[70%] py-2 px-4 border border-blue-900 rounded-lg'>
+                <div className='flex gap-1 justify-center items-center font-semibold cursor-pointer'>
+                    <BookCheck/>
+                    <span>To do</span>
+                </div>
+            <button className='flex gap-1 justify-center items-center bg-blue-950 p-2 rounded-lg font-semibold cursor-pointer'>
+                <Plus/>
+            </button>
+        </header>
+        </div>
+        </>
+    )
+}
+
+export default Header;
